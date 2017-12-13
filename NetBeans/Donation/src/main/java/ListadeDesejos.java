e630dfe0b4986d49f797340870dbace69a37e8bd
Lucas <sonserina.nao.drago@gmail.com>
2017-12-13 11:28:57 -0200

    Commit do NetBeans com Maven

--- NetBeans/Donation/src/main/java/ListadeDesejos.java
+++ NetBeans/Donation/src/main/java/ListadeDesejos.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package donation;
+
 
 /**
  *
  * @author aluno
  */
-public class Lista_de_desejos {
+public class ListadeDesejos {
 
     /**
      * @return the rascunho
